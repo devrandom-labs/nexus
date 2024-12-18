38dba890dd04e4a3a689f710f1034bddb9b971af
Joel DSouza <joeldsouzax@gmail.com>
2024-12-18 14:33:42 +0100

    chore: added location and coordinates

--- bins/events/src/domains/location.rs
+++ bins/events/src/domains/location.rs
@@ -1,1 +1,14 @@
-pub struct Location {}
+pub struct Location {
+    address: Address,
+    coordinates: Option<Coordinates>,
+}
+
+pub struct Address {
+    line_1: String,
+    line_2: Option<String>,
+}
+
+pub struct Coordinates {
+    latitude: f64,
+    longitude: f64,
+}
