b0e7faeaa89e7b84876c6af0b4e588e3d5ca9cd3
Joel DSouza <joeldsouzax@gmail.com>
2024-12-28 19:54:48 +0100

    feat(events): added event id domain

--- bins/events/src/domain/event.rs
+++ bins/events/src/domain/event.rs
@@ -1,8 +1,43 @@
 #[derive(Debug)]
-pub struct EventId(String);
+pub enum EventError {
+    InvalidId,
+}
+
+#[derive(Debug)]
+pub struct EventId(pub String);
+
+impl EventId {
+    pub fn new(id: String) -> Result<Self, EventError> {
+        if id.is_empty() {
+            return Err(EventError::InvalidId);
+        }
+
+        Ok(EventId(id))
+    }
+}
 
 #[derive(Debug)]
 pub struct Event {
     id: EventId,
     title: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_create_event_id() {
+        let id = "some_id".to_string();
+
+        let event_id = EventId::new(id.clone()).unwrap();
+        assert_eq!(id, event_id.0);
+    }
+
+    #[test]
+    fn event_id_should_not_be_created_if_empty() {
+        let id = "".to_string();
+        let event_id = EventId::new(id.clone());
+        assert!(event_id.is_err());
+    }
 }
