ba46a0791ba94ad2bc30d5bf6a27bb18a230d9aa
Joel DSouza <joeldsouzax@gmail.com>
2025-01-03 23:18:45 +0100

    refactor(events): removed all previous code

--- bins/events/src/main.rs
+++ bins/events/src/main.rs
@@ -1,15 +1,46 @@
-mod application;
-mod cqrs;
-mod domain;
-mod infra;
-
-fn main() {
-    println!("hello world events");
+pub enum EventStatus {
+    Draft,
+    Cancelled,
+    Published,
+    Completed,
+    Deleted,
 }
 
-#[cfg(test)]
-mod tests {
-
-    #[test]
-    fn default_just_to_pass_checks_events() {}
+pub struct EventAggregate {
+    id: String,
+    title: String,
+    status: EventStatus,
 }
+
+pub enum Commands {
+    CreateEvent { id: String, title: String },
+    DeleteEvent { id: String },
+}
+
+pub enum Events {
+    EventCreated {
+        id: String,
+        title: String,
+        status: EventStatus,
+    },
+    EventDeleted {
+        id: String,
+        status: EventStatus,
+    },
+}
+
+impl EventAggregate {
+    pub fn apply(&mut self, events: Events) {
+        match events {
+            Events::EventCreated { title, status, .. } => {
+                self.title = title;
+                self.status = status;
+            }
+            Events::EventDeleted { status, .. } => {
+                self.status = status;
+            }
+        }
+    }
+}
+
+fn main() {}
