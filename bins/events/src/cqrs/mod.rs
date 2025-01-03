482553bb121ca8faf61aae879748d38e3297f9b3
Joel DSouza <joeldsouzax@gmail.com>
2025-01-03 15:14:47 +0100

    feat(events): added an example aggregate model

--- bins/events/src/cqrs/mod.rs
+++ bins/events/src/cqrs/mod.rs
@@ -1,7 +1,8 @@
-pub trait Aggregate: Send + Sync {
+pub trait Aggregate {
     type Commands;
     type Events;
     type Errors;
+    fn get_aggregate() -> String;
     fn handle(&self, commands: Self::Commands) -> Result<Vec<Self::Events>, Self::Errors>;
     fn apply(&mut self, event: Self::Events);
 }
--- bins/events/src/domain/mod.rs
+++ bins/events/src/domain/mod.rs
@@ -1,1 +1,25 @@
+use crate::application::EventCommand;
+use crate::application::EventEvent;
+use crate::cqrs::Aggregate;
+
 pub mod event;
+
+pub enum EventErrors {}
+
+pub struct EventAggregate;
+
+impl Aggregate for EventAggregate {
+    type Commands = EventCommand;
+    type Events = EventEvent;
+    type Errors = EventErrors;
+    fn handle(&self, commands: Self::Commands) -> Result<Vec<Self::Events>, Self::Errors> {
+        match commands {
+            EventCommand::CreateEvent => Ok(vec![EventEvent::EventCreated]),
+            EventCommand::DeleteEvent => Ok(vec![EventEvent::EventDeleted]),
+        }
+    }
+    fn apply(&mut self, _event: Self::Events) {}
+    fn get_aggregate() -> String {
+        "event".to_string()
+    }
+}
