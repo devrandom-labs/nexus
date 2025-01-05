242620ce4a5384f4afc5cb5cdd9bec6f007a93be
Joel DSouza <joeldsouzax@gmail.com>
2025-01-05 21:04:15 +0100

    feat(events): added tracing config

--- bins/events/src/main.rs
+++ bins/events/src/main.rs
@@ -1,5 +1,22 @@
-use tracing::instrument;
+use tracing::{info, instrument};
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    prelude::*,
+    EnvFilter, Layer,
+};
 mod events;
 
 #[instrument]
-fn main() {}
+fn main() {
+    let filter = EnvFilter::from_default_env();
+    let console = fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_level(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_filter(filter);
+    tracing_subscriber::registry().with(console).init();
+    info!("🚀🚀🎆{}🎆🚀🚀", "tixlys event systems");
+}
