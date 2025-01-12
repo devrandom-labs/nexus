5062103ba7316608646b5385f8669d9fba6404e7
Joel DSouza <joeldsouzax@gmail.com>
2025-01-12 12:34:52 +0100

    feat(cqrs): added stream_id to event envelope

--- crates/cqrs/src/event.rs
+++ crates/cqrs/src/event.rs
@@ -1,5 +1,6 @@
 use crate::aggregate::Aggregate;
 use std::collections::HashMap;
+use ulid::Ulid;
 
 pub trait DomainEvent {
     fn event_type(&self) -> String;
@@ -11,6 +12,7 @@ pub struct EventEnvelope<A>
 where
     A: Aggregate,
 {
+    pub stream_id: Ulid,
     pub payload: A::Event,
     pub metadata: HashMap<String, String>,
 }
