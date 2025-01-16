b13343d56e46f8c849fb2e7cb3387a0ae0846016
Joel DSouza <joeldsouzax@gmail.com>
2025-01-16 22:29:47 +0100

    feat(events): adding command channel

--- bins/events/src/commander.rs
+++ bins/events/src/commander.rs
@@ -1,14 +1,1 @@
-trait CommandDispatcher {
-    type Commands;
-    type Error;
-    fn dispatch(command: Self::Commands) -> Result<(), Self::Error>;
-}
-
-trait Commandable {} //  tokio service
-
-trait CommandHandler {
-    type Dispatcher: CommandDispatcher;
-    type Commands: Commandable;
-    fn handle(commands: &[Self::Commands]) -> Self;
-    fn run(self) -> Result<Self::Dispatcher, String>;
-}
+use tokio::sync::mpsc::unbounded_channel;
