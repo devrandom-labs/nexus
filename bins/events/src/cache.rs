a6a19043b0966c4d3426cbb3e22f614bb3e53236
Joel DSouza <joeldsouzax@gmail.com>
2025-01-19 20:05:00 +0100

    feat(events): added cache impl

--- bins/events/src/cache.rs
+++ bins/events/src/cache.rs
@@ -8,6 +8,7 @@ pub enum Error {
 }
 
 pub struct Cache<C> {
+    capacity: usize,
     _type: PhantomData<C>,
 }
 
@@ -17,4 +18,18 @@ where
 {
     fn get(&self) -> C;
     fn put(&mut self, data: C) -> Result<C, Error>;
+}
+
+impl<C> Cache<C> {
+    pub fn new(capacity: usize) -> Self {
+        Cache {
+            capacity,
+            _type: PhantomData,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
