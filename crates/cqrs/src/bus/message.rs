5c33c5111dd43c3654d5b1006081818c7eedbb85
Joel DSouza <joeldsouzax@gmail.com>
2025-01-21 17:53:23 +0100

    feat(cqrs): messages derive debug traits

--- crates/cqrs/src/bus/message.rs
+++ crates/cqrs/src/bus/message.rs
@@ -1,10 +1,16 @@
+use std::fmt::Debug;
 use tokio::sync::oneshot::Sender;
 
 /// payload should not be dynamic dispatch,
 /// sum type of messages should be sent through the messages,
 ///
 /// but reply can transfer any kind of type back, so it can be dynamic dispatch
-pub struct Message<T, R> {
+#[derive(Debug)]
+pub struct Message<T, R>
+where
+    R: Debug,
+    T: Debug,
+{
     reply: Sender<R>,
     payload: T,
 }
