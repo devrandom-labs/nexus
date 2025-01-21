d4a2a7ee76c54722d90957144ba35f02c07b3dd2
Joel DSouza <joeldsouzax@gmail.com>
2025-01-21 17:51:39 +0100

    feat(cqrs): added message and clear types

--- crates/cqrs/src/bus/executor.rs
+++ crates/cqrs/src/bus/executor.rs
@@ -1,1 +1,4 @@
-pub struct Executor {}
+use super::message::Message;
+use tokio::sync::mpsc::Sender;
+
+pub struct Executor<T>(Sender<Message<T>>);
--- crates/cqrs/src/bus/message.rs
+++ crates/cqrs/src/bus/message.rs
@@ -1,3 +1,10 @@
-pub struct Message<T> {
+use tokio::sync::oneshot::Sender;
+
+/// payload should not be dynamic dispatch,
+/// sum type of messages should be sent through the messages,
+///
+/// but reply can transfer any kind of type back, so it can be dynamic dispatch
+pub struct Message<T, R> {
+    reply: Sender<R>,
     payload: T,
 }
