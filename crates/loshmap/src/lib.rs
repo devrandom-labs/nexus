7cbbe5fcc2d6b60c78c1e5bfaa7e1d52c4d6b275
Joel DSouza <joeldsouzax@gmail.com>
2025-01-22 13:50:36 +0100

    feat(tixlys): removed loshmap for now

--- crates/loshmap/src/lib.rs
+++
@@ -1,6 +0,0 @@
-/// The largest possible table capacity. This value must be
-const MAXIMUM_CAPACITY: usize = 1 << 30;
-
-/// The default initial table capacity. Must be a power of 2
-/// (i.e., at least 1) and at most `MAXIMUM_CAPACITY`.
-const DEFAULT_CAPACITY: usize = 16;
