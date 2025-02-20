a49a43c2208c8fcea1e39562057241197ed80e76
Joel DSouza <joeldsouzax@gmail.com>
2025-02-20 13:04:07 +0100

    feat(cqrs): messages are thread safe rr

--- crates/cqrs/src/bus_two/mod.rs
+++ crates/cqrs/src/bus_two/mod.rs
@@ -1,5 +1,12 @@
 use thiserror::Error as Err;
-pub mod message_handler;
 
 #[derive(Err, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Error {}
+
+// Trait Object, TypeId, 'static lifetime messages and runtime type reflection.
+use std::any::Any;
+
+pub trait Message: Any + Send + Sync + 'static {}
+
+// blanket impl, I think this is okay for now.
+impl<T: Any + Send + Sync + 'static> Message for T {}
