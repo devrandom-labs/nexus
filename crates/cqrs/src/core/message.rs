146914568f70b8716c528dbc7edaf2406274687e
Joel DSouza <joeldsouzax@gmail.com>
2025-03-10 21:24:19 +0100

    feat: dedup, time_stamp for messages

--- crates/cqrs/src/core/message.rs
+++ crates/cqrs/src/core/message.rs
@@ -1,21 +1,28 @@
 use super::body::Body;
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
+use std::time::Instant;
 
-// TODO: add hash to messages, since they would use rkyv for this
 pub struct Message {
+    dedup_id: u32,
     header: HashMap<String, String>,
     body: Body,
+    time_stamp: Instant,
 }
 
 impl Message {
-    pub fn new<T>(body: T) -> Self
+    pub fn new<T>(dedup_id: u32, body: T) -> Self
     where
         T: Any + Send + Sync,
     {
         let body = Body::new(body);
         let header = HashMap::new();
-        Message { body, header }
+        Message {
+            dedup_id,
+            body,
+            header,
+            time_stamp: Instant::now(),
+        }
     }
 
     pub fn type_id(&self) -> TypeId {
@@ -36,34 +43,4 @@ impl Message {
 }
 
 #[cfg(test)]
-mod test {
-
-    use super::Message;
-
-    // get the type from body
-
-    #[test]
-    pub fn body_type_id_should_be_different_for_different_types() {
-        let message_one = Message::new(String::from("Hello"));
-        let message_two = Message::new(12);
-        assert_ne!(message_one.type_id(), message_two.type_id());
-    }
-
-    #[test]
-    pub fn body_type_id_should_be_same_for_same_types() {
-        let message_one = Message::new(String::from("Hello"));
-        let message_two = Message::new(String::from("Joel"));
-        assert_eq!(message_one.type_id(), message_two.type_id());
-    }
-
-    #[tokio::test]
-    pub async fn body_should_be_transferable_between_threads_and_tasks() {
-        let message_one = Message::new(String::from("Hello"));
-        let result = tokio::spawn(async move {
-            let message_two = Message::new(String::from("Joel"));
-            assert_eq!(message_one.type_id(), message_two.type_id());
-        })
-        .await;
-        assert!(result.is_ok());
-    }
-}
+mod test {}
