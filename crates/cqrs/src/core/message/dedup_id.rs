558efaebe4910797e00db60e6fe4c147a32da667
Joel DSouza <joeldsouzax@gmail.com>
2025-03-12 13:23:47 +0100

    test(cqrs): added message tests

--- crates/cqrs/src/core/message/dedup_id.rs
+++
@@ -1,5 +0,0 @@
-pub struct DedupId(u32);
-
-// TODO: dedup takes some data and creates a hash
-// TODO: hash creation should be ultra performant.
-// TODO: hash should be tiny and have less memory footprint
--- crates/cqrs/src/core/message/error.rs
+++
@@ -1,7 +0,0 @@
-use thiserror::Error as Err;
-
-#[derive(Debug, Err)]
-pub enum Error {
-    #[error("Could not create dedup id")]
-    CouldNotCreateDedupId,
-}
