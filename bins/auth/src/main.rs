4766fa202313032e5f77fa6ede6d3833373b2dc4
Joel DSouza <joeldsouzax@gmail.com>
2025-03-13 10:28:09 +0100

    feat(auth): added basic axum router

--- bins/auth/src/main.rs
+++ bins/auth/src/main.rs
@@ -1,4 +1,36 @@
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tracing::{error, info, instrument};
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    prelude::*,
+    EnvFilter, Layer,
+};
+
+#[instrument]
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let filter = EnvFilter::from_default_env();
+    let console = fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_level(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_filter(filter);
+    tracing_subscriber::registry().with(console).init();
+
+    let workspace = "tixlys";
+    let name = env!("CARGO_BIN_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    info!("🚀🚀🎆{}:{}@{}🎆🚀🚀", workspace, name, version);
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .inspect_err(|err| error!(?err))
+        .unwrap();
+    axum::serve(listener, app)
+        .await
+        .inspect_err(|err| error!("🚫{:?}🚫", err))
+        .unwrap();
 }
