7a21a8e1a55b24a4bd1e7222555fdd411d632eba
Joel DSouza <joeldsouzax@gmail.com>
2025-03-14 22:10:25 +0100

    feat(steersman): added tracing

--- bins/steersman/src/main.rs
+++ bins/steersman/src/main.rs
@@ -1,6 +1,27 @@
+use tracing::{info, instrument};
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    prelude::*,
+    EnvFilter, Layer,
+};
+
+#[instrument]
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let filter = EnvFilter::from_default_env();
+    let console = fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_level(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_filter(filter);
+    tracing_subscriber::registry().with(console).init();
+
+    let workspace = "tixlys";
+    let name = env!("CARGO_BIN_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    info!("🚀🚀🎆{}:{}@{}🎆🚀🚀", workspace, name, version);
 }
 
-// TODO: integrate pingora and test out request for http
+// TODO: build this docker image add file and check if the file is showing
