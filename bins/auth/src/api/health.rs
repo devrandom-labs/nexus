158cecc758cb131cb0c8638ffb6734b739688778
Joel DSouza <joeldsouzax@gmail.com>
2025-03-21 15:56:17 +0100

    feat(auth): doc for health endpoint

--- bins/auth/src/api/health.rs
+++ bins/auth/src/api/health.rs
@@ -1,4 +1,45 @@
-#[utoipa::path(get, path = "/health", responses((status = OK, body = String, description = "Check Application Health")))]
+/// Health Check Endpoint
+///
+/// This endpoint performs a basic health check on the application. It verifies that the server is running and responsive.
+///
+/// **Purpose:**
+///
+/// * Allows monitoring systems to verify the application's availability.
+/// * Provides a simple way to check if the application is functioning correctly.
+///
+/// **Response:**
+///
+/// * On success, returns a 200 OK status with the string "ok." in the response body.
+/// * The response indicates that the application is running and healthy.
+///
+/// **Usage:**
+///
+/// This endpoint should be used by monitoring services or developers to quickly assess the application's status. It does not require any authentication or input parameters.
+///
+/// **Example Request:**
+///
+/// ```http
+/// GET /health
+/// ```
+///
+/// **Example Response:**
+///
+/// ```http
+/// HTTP/1.1 200 OK
+/// Content-Type: text/plain
+///
+/// ok.
+#[utoipa::path(get,
+               path = "/health",
+               tag = "Infra",
+               operation_id = "healthCheck",
+               responses(
+                   (status = OK, body = String, description = "Check Application Health")
+               )
+)]
 pub async fn route() -> &'static str {
     "ok."
 }
+
+#[cfg(test)]
+mod tests {}
--- bins/auth/src/api/login.rs
+++ bins/auth/src/api/login.rs
@@ -1,4 +1,4 @@
-#[utoipa::path(post, path = "/login", responses((status = OK, body = String, description = "Login User")))]
+#[utoipa::path(post, path = "/login", tags = ["User Authentication"], operation_id = "loginUser", responses((status = OK, body = String, description = "Login User")))]
 pub async fn route() -> &'static str {
     "login"
 }
