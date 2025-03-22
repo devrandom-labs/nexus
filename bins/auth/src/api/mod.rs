3deefb72958d4624e83dc752c87245f607e63960
Joel DSouza <joeldsouzax@gmail.com>
2025-03-22 23:56:44 +0100

    test(auth): health router check

--- bins/auth/src/api/mod.rs
+++ bins/auth/src/api/mod.rs
@@ -25,3 +25,36 @@ pub struct ApiDoc;
 // TODO: improve open api documentation
 // TODO: add security add on for login route
 // TODO: test all apis
+//
+#[cfg(test)]
+mod tests {
+    use super::router;
+    use axum::{
+        Router,
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use serde_json::{Value, json};
+    use tower::ServiceExt;
+
+    pub fn get_router() -> Router {
+        router().split_for_parts().0
+    }
+
+    #[tokio::test]
+    async fn health_check() {
+        let router = get_router();
+        let health_request = Request::builder()
+            .uri("/health")
+            .body(Body::empty())
+            .unwrap();
+
+        let response = router.oneshot(health_request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({"message": "ok."}))
+    }
+}
