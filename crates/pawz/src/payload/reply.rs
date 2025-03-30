0d0e1976ac95b0a7b3813c93d7453a6a931a030d
Joel DSouza <joeldsouzax@gmail.com>
2025-03-30 21:28:58 +0200

    test(pawz): added some tests

--- crates/pawz/src/payload/reply.rs
+++ crates/pawz/src/payload/reply.rs
@@ -119,21 +119,16 @@ impl Default for Reply<Fail<String>> {
     }
 }
 
-impl<T> Reply<T>
-where
-    T: ReplyType,
-{
-    pub fn error() -> Reply<Error<String>> {
-        Reply::<Error<String>>::default()
-    }
+pub fn error() -> Reply<Error<String>> {
+    Reply::<Error<String>>::default()
+}
 
-    pub fn success() -> Reply<Success<String>> {
-        Reply::<Success<String>>::default()
-    }
+pub fn success() -> Reply<Success<String>> {
+    Reply::<Success<String>>::default()
+}
 
-    pub fn fail() -> Reply<Fail<String>> {
-        Reply::<Fail<String>>::default()
-    }
+pub fn fail() -> Reply<Fail<String>> {
+    Reply::<Fail<String>>::default()
 }
 
 impl<T> Reply<Error<T>>
@@ -190,5 +185,129 @@ where
 
     pub fn build(self) -> Body<T> {
         Body::success(self.r#type.body)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Serialize, PartialEq, Debug)]
+    struct TestData {
+        value: i32,
+    }
+
+    #[test]
+    fn test_default_error() {
+        let reply = error();
+        let body = reply.build();
+        assert_eq!(
+            body,
+            Body::error(
+                "internal server error",
+                Some(StatusCode::INTERNAL_SERVER_ERROR.as_u16())
+            )
+        );
+    }
+
+    #[test]
+    fn test_error_with_body() {
+        let data = TestData { value: 42 };
+        let reply = error().with_body(data);
+        let body = reply.build();
+        assert_eq!(
+            body,
+            Body::error_with_body(
+                "internal server error",
+                Some(StatusCode::INTERNAL_SERVER_ERROR.as_u16()),
+                Some(TestData { value: 42 })
+            )
+        );
+    }
+
+    #[test]
+    fn test_error_with_message() {
+        let reply = error().with_message("custom error".to_string());
+        let body = reply.build();
+        assert_eq!(
+            body,
+            Body::error(
+                "custom error",
+                Some(StatusCode::INTERNAL_SERVER_ERROR.as_u16())
+            )
+        );
+    }
+
+    #[test]
+    fn test_error_with_code() {
+        let reply = error().with_code(StatusCode::NOT_FOUND.as_u16());
+        let body = reply.build();
+        assert_eq!(
+            body,
+            Body::error(
+                "internal server error",
+                Some(StatusCode::NOT_FOUND.as_u16())
+            )
+        );
+    }
+
+    #[test]
+    fn test_default_success() {
+        let reply = success();
+        let body = reply.build();
+        assert_eq!(body, Body::success("ok.".to_string()));
+    }
+
+    #[test]
+    fn test_success_with_body() {
+        let data = TestData { value: 42 };
+        let reply = success().with_body(data);
+        let body = reply.build();
+        assert_eq!(body, Body::success(TestData { value: 42 }));
+    }
+
+    #[test]
+    fn test_default_fail() {
+        let reply = fail();
+        let body = reply.build();
+        assert_eq!(body, Body::fail("not ok".to_string()));
+    }
+
+    #[test]
+    fn test_fail_with_body() {
+        let data = TestData { value: 42 };
+        let reply = fail().with_body(data);
+        let body = reply.build();
+        assert_eq!(body, Body::fail(TestData { value: 42 }));
+    }
+
+    #[test]
+    fn test_generic_error_with_json_body_using_error_function() {
+        let data = TestData { value: 42 };
+        let reply = error()
+            .with_body(data)
+            .with_message("test error".to_string())
+            .with_code(400);
+        let body = reply.build();
+        assert_eq!(
+            body,
+            Body::error_with_body("test error", Some(400), Some(TestData { value: 42 }))
+        );
+    }
+
+    #[test]
+    fn test_generic_success_with_json_body_using_success_function() {
+        let data = TestData { value: 42 };
+        let reply = success().with_body(data);
+        let body = reply.build();
+        assert_eq!(body, Body::success(TestData { value: 42 }));
+    }
+
+    #[test]
+    fn test_generic_fail_with_json_body_using_fail_function() {
+        let data = TestData { value: 42 };
+        let reply = fail().with_body(data);
+        let body = reply.build();
+        assert_eq!(body, Body::fail(TestData { value: 42 }));
     }
 }
