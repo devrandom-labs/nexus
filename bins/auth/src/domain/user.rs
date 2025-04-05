08eff23973273dff606d997a5618cb36b7c8583c
Joel DSouza <joeldsouzax@gmail.com>
2025-04-06 00:25:39 +0200

    feat(auth): added cqrs to auth

--- bins/auth/src/domain/user.rs
+++ bins/auth/src/domain/user.rs
@@ -2,6 +2,7 @@
 use super::email::Email;
 use super::password::Password;
 use super::user_id::UserId;
+use cqrs::aggregate::Aggregate;
 
 #[derive(Debug)]
 pub enum User {
@@ -16,11 +17,4 @@ pub enum User {
         email: Email,
         password: Password,
     },
-}
-
-pub trait Aggregate {
-    type Root;
-    type Events;
-    type Error: std::error::Error;
-    fn apply(state: Option<Self::Root>, events: &Self::Events) -> Result<Self::Root, Self::Error>;
 }
--- crates/cqrs/src/aggregate.rs
+++ crates/cqrs/src/aggregate.rs
@@ -1,5 +1,6 @@
 pub trait Aggregate {
-    type Error: std::error::Error;
-    type Commands;
+    type Root;
     type Events;
+    type Error: std::error::Error;
+    fn apply(state: Option<Self::Root>, events: &Self::Events) -> Result<Self::Root, Self::Error>;
 }
