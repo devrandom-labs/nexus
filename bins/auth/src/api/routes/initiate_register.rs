36582adfe41688eb9ec70e1156b73bfe4ddedbef
Joel DSouza <joeldsouzax@gmail.com>
2025-04-08 00:38:28 +0200

    feat(auth): added initiate email request

--- bins/auth/src/api/routes/initiate_register.rs
+++ bins/auth/src/api/routes/initiate_register.rs
@@ -1,1 +1,12 @@
 // TODO: takes only an email address
+use serde::Deserialize;
+use utoipa::ToSchema;
+use validator::Validate;
+
+#[derive(Debug, Deserialize, Validate, ToSchema)]
+pub struct InitiateRequest {
+    /// The user's email address.
+    #[schema(example = "joel@tixlys.com")]
+    #[validate(email(message = "Invalid email format"))]
+    email: String,
+}
--- bins/auth/src/domain/mod.rs
+++ bins/auth/src/domain/mod.rs
@@ -1,7 +1,6 @@
 use thiserror::Error as TError;
 
 pub(super) mod email;
-pub(super) mod password;
 pub(super) mod user;
 pub(super) mod user_id;
 
