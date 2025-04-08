fdcae4944b325e056655920f00287741d7cf1d57
Joel DSouza <joeldsouzax@gmail.com>
2025-04-08 11:01:31 +0200

    feat(auth): added login,verify

--- bins/auth/src/api/routes/login.rs
+++ bins/auth/src/api/routes/login.rs
@@ -1,3 +1,13 @@
+use serde::Deserialize;
+use utoipa::ToSchema;
+use validator::Validate;
+
+#[derive(Deserialize, Validate, ToSchema)]
+pub struct LoginRequest {
+    email: String,
+    password: String,
+}
+
 #[utoipa::path(post, path = "/login", tags = ["User Authentication"], operation_id = "loginUser", responses((status = OK, body = String, description = "Login User")))]
 pub async fn route() -> &'static str {
     "login"
--- bins/auth/src/api/routes/verify_email.rs
+++ bins/auth/src/api/routes/verify_email.rs
@@ -1,1 +1,14 @@
+use serde::Deserialize;
+use utoipa::ToSchema;
+use validator::Validate;
 
+#[derive(Debug, Deserialize, Validate, ToSchema)]
+pub struct VerifyEmailRequest {
+    /// The user's email address.
+    #[schema(example = "joel@tixlys.com")]
+    #[validate(email(message = "Invalid email format"))]
+    email: String,
+    otp: String,
+}
+
+// TODO: figure out the flow for this, verify first methodolgy
