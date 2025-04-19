f1dfbc857d68e1053d945e120fa04796fd099b31
Joel DSouza <joeldsouzax@gmail.com>
2025-04-20 00:06:38 +0200

    feat(nexus): added respositor error

--- crates/nexus/src/repository/mod.rs
+++ crates/nexus/src/repository/mod.rs
@@ -1,14 +1,35 @@
-use crate::aggregate::{AggregateRoot, AggregateType};
-use std::{fmt::Debug, future::Future, pin::Pin};
+use crate::aggregate::{AggregateRoot, AggregateType, aggregate_root::AggregateLoadError};
+use std::{error::Error as StdError, fmt::Debug, future::Future, hash::Hash, pin::Pin};
 use thiserror::Error as ThisError;
+use tower::BoxError;
 
 #[derive(Debug, ThisError)]
 pub enum RepositoryError<Id>
 where
-    Id: Debug,
+    Id: Debug + Send + Sync + Hash + Eq + Clone + 'static,
+    AggregateLoadError<Id>: StdError + Send + Sync + 'static,
 {
     #[error("Aggregate with ID '{0:?}' not found")]
     AggregateNotFound(Id),
+
+    #[error("Failed to deserialize event data from store")]
+    DeserializationError {
+        #[source]
+        source: BoxError,
+    },
+
+    #[error("Failed to read from event store")]
+    StoreError {
+        #[source]
+        source: BoxError,
+    },
+
+    #[error("Loaded data integrity issue for aggregator '{aggregate_id:?}'")]
+    DataIntegrityError {
+        aggregate_id: Id,
+        #[source]
+        source: AggregateLoadError<Id>,
+    },
 }
 
 pub trait EventSourceRepository: Send + Sync + Clone {
