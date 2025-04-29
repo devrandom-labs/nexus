3550a56741dc2685b033162722c67eac77937570
Joel DSouza <joeldsouzax@gmail.com>
2025-04-29 23:44:15 +0200

    fix(nexus): fixed handler register bug

--- crates/nexus/src/query/dispatcher.rs
+++ crates/nexus/src/query/dispatcher.rs
@@ -3,8 +3,7 @@ use crate::{Query, error::Error};
 use std::{
     any::{Any, TypeId},
     boxed::Box,
-    collections::HashMap,
-    default::Default,
+    collections::{HashMap, hash_map::Entry},
     marker::PhantomData,
     pin::Pin,
     sync::Arc,
@@ -14,7 +13,7 @@ use tower::{BoxError, Service, ServiceExt};
 
 pub type BoxMessage = Box<dyn Any + Send>;
 pub type ErasedResult = Result<BoxMessage, BoxError>;
-pub type ErasedFuture = Pin<Box<dyn Future<Output = ErasedResult> + Send>>;
+pub type ErasedFuture = Pin<Box<dyn Future<Output = ErasedResult> + Send + 'static>>;
 
 pub trait ErasedQueryHandlerFn: Send + Sync {
     fn call(&self, query: BoxMessage) -> ErasedFuture;
@@ -64,26 +63,19 @@ where
             let result: Q::Result = ready_service
                 .call(*query)
                 .await
-                .map_err(|e| BoxError::from(Error::<Q::Error>::HandlerFailed(e.into())))?;
+                .map_err(|e| BoxError::from(Error::<Q::Error>::HandlerFailed(e)))?;
             Ok(Box::new(result) as BoxMessage)
         })
     }
 }
 
 #[derive(Debug, ThisError)]
-#[error("Could not register service")]
-pub struct RegistrationFailed;
+#[error("Registration Failed: {0}")]
+pub struct RegistrationFailed(String);
 
+#[derive(Default)]
 pub struct QueryDispatcherBuilder {
     services: HashMap<TypeId, Arc<dyn ErasedQueryHandlerFn>>,
-}
-
-impl Default for QueryDispatcherBuilder {
-    fn default() -> Self {
-        Self {
-            services: HashMap::new(),
-        }
-    }
 }
 
 impl QueryDispatcherBuilder {
@@ -98,12 +90,22 @@ impl QueryDispatcherBuilder {
         S::Future: Send + Sync,
     {
         let query_type_id = TypeId::of::<Q>();
-        let query_name = std::any::type_name::<Q>();
         let dispatcher_services: Arc<dyn ErasedQueryHandlerFn> =
             Arc::new(DispatcherQueryHandler::<Q, S>::new(service));
-        self.services
-            .insert(query_type_id, dispatcher_services)
-            .ok_or(RegistrationFailed)?;
+
+        match self.services.entry(query_type_id) {
+            Entry::Occupied(_) => {
+                let query_name = std::any::type_name::<Q>();
+                Err(RegistrationFailed(format!(
+                    "Service exists for {} query",
+                    query_name
+                )))
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(dispatcher_services);
+                Ok(())
+            }
+        }?;
         Ok(self)
     }
 
