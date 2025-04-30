6ccb5b38f6f10ebc8a9e0ab3cbc965629f7030a8
Joel DSouza <joeldsouzax@gmail.com>
2025-04-30 14:27:53 +0200

    chore(nexus): added command dispatcher builder

--- crates/nexus/src/command/dispatcher.rs
+++ crates/nexus/src/command/dispatcher.rs
@@ -1,1 +1,13 @@
+#![allow(dead_code)]
+use super::repository::EventSourceRepository;
+use std::sync::Arc;
 
+#[derive(Default)]
+pub struct CommandDispatcherBuilder<R, AppServices>
+where
+    R: EventSourceRepository + 'static,
+    AppServices: Send + Sync + Clone + 'static,
+{
+    repository: Option<Arc<R>>,
+    shared_services: Option<Arc<AppServices>>,
+}
