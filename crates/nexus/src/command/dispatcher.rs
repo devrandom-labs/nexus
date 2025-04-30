1b7444159116a8dcade304a349bb3e4bc9ec4a8e
Joel DSouza <joeldsouzax@gmail.com>
2025-04-30 14:34:52 +0200

    feat(nexus): added repo and shared services

--- crates/nexus/src/command/dispatcher.rs
+++ crates/nexus/src/command/dispatcher.rs
@@ -11,3 +11,26 @@ where
     repository: Option<Arc<R>>,
     shared_services: Option<Arc<AppServices>>,
 }
+
+impl<R, AppServices> CommandDispatcherBuilder<R, AppServices>
+where
+    R: EventSourceRepository + 'static,
+    AppServices: Send + Sync + Clone + 'static,
+{
+    pub fn new() -> Self {
+        CommandDispatcherBuilder {
+            repository: None,
+            shared_services: None,
+        }
+    }
+
+    pub fn repository(mut self, repository: R) -> Self {
+        self.repository = Some(Arc::new(repository));
+        self
+    }
+
+    pub fn shared_services(mut self, shared_services: AppServices) -> Self {
+        self.shared_services = Some(Arc::new(shared_services));
+        self
+    }
+}
