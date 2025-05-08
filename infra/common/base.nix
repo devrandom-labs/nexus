43aefa1ec42b3861514b75282563ccc0d2bbf82c
Joel DSouza <joeldsouzax@gmail.com>
2025-05-09 01:44:44 +0200

    feat(tixlys): added openssh

--- infra/common/base.nix
+++ infra/common/base.nix
@@ -5,5 +5,23 @@
     isNormalUser = true;
     description = "Admin user for tixlys servers";
     extraGroups = [ "wheel" "podman" ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICuNMvZ7mBPTbASlv7Dg4By/07XW+fs9i+KkYh5xUDWQ joeldsouzax@gmail.com"
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = true;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 80 443 ];
   };
 }
