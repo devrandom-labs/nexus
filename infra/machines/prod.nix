a4d84ca62e895adca4d6684ca006de25fc07c139
Joel DSouza <joeldsouzax@gmail.com>
2025-05-09 01:17:37 +0200

    chore(tixlys): added basic prod server config

--- infra/machines/prod.nix
+++ infra/machines/prod.nix
@@ -1,4 +1,20 @@
-{config, lib, pkgs}:
-{
+{ config, lib, pkgs }: {
 
+  imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
+  system.stateVersion = "24.11";
+  time.timeZone = "Etc/UTC";
+
+  # --- Cloud VM Specifics ---
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+
+  # --- Filesystem Configuration ---
+  fileSystems."/" = {
+    device = "/dev/vda1";
+    fsType = "ext4";
+  };
+
+  # --- Networking Configuration ---
+  networking.useDHCP = true;
+  networking.hostName = "tixlys-local-test";
 }
