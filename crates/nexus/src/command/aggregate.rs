3d64191a107b0e8a66b8b00933f0feaebf3772a4
Joel DSouza <joeldsouzax@gmail.com>
2025-05-20 15:35:38 +0200

    refactor(nexus): swapped state traits with event

--- crates/nexus/src/command/aggregate.rs
+++ crates/nexus/src/command/aggregate.rs
@@ -4,11 +4,10 @@ use serde::{Serialize, de::DeserializeOwned};
 use std::{fmt::Debug, hash::Hash};
 use thiserror::Error as ThisError;
 
-/// Represents the actual state data of an aggregate.
-/// It's rebuilt by applying events and must know how to apply them.
+/// # `AggregateState`
 pub trait AggregateState: Default + Send + Sync + Debug + 'static {
     /// Specific type of Domain Event this state reacts to.
-    type Event: Message + Clone + Serialize + DeserializeOwned;
+    type Event: DomainEvent;
 
     /// Mutates the state based on a receieved event.
     /// This is the core of state reconstruction in Event Sourcing.
