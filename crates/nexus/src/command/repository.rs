0424999a78aee1d96519f3062b1e1e69759661f5
Joel DSouza <joeldsouzax@gmail.com>
2025-06-01 17:07:02 +0200

    test(nexus): added aggregate not found error

--- crates/nexus/src/command/repository.rs
+++ crates/nexus/src/command/repository.rs
@@ -174,12 +174,26 @@ pub trait EventSourceRepository: Send + Sync + Clone + Debug {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::command::test::mocks::MockRepository;
 
     #[tokio::test]
     async fn should_load_aggregate_when_correct_id_is_provided() {}
 
     #[tokio::test]
-    async fn should_give_aggregate_not_found_error_when_invalid_id_is_provided() {}
+    async fn should_give_aggregate_not_found_error_when_invalid_id_is_provided() {
+        let repo = MockRepository::new();
+        let id = "id".to_string();
+        let user_aggregate = repo.load(&id).await;
+        assert!(user_aggregate.is_err());
+        let error = user_aggregate.unwrap_err();
+        match error {
+            RepositoryError::AggregateNotFound(error_id) => {
+                assert_eq!(error_id, id);
+            }
+            _ => panic!("expected AggregateNotFoundError"),
+        }
+    }
 
     #[tokio::test]
     async fn should_save_aggregate_uncommited_events() {}
