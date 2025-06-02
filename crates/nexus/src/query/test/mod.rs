e82147934cf485547f540a04ac8878f8290ed5fb
Joel DSouza <joeldsouzax@gmail.com>
2025-06-02 13:48:34 +0200

    test(nexus): added read model

--- crates/nexus/src/query/test/mod.rs
+++ crates/nexus/src/query/test/mod.rs
@@ -1,1 +1,15 @@
+use super::model::ReadModel;
 
+// Read model
+pub struct User {
+    id: String,
+    pub email: String,
+}
+
+impl ReadModel for User {
+    type Id = String;
+
+    fn id(&self) -> &Self::Id {
+        &self.id
+    }
+}
