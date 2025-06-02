9970af10b98c2fb30d0c6da5e3f818ac6613e5fe
Joel DSouza <joeldsouzax@gmail.com>
2025-06-02 13:55:34 +0200

    test(nexus): added get user repository

--- crates/nexus/src/query/test/mod.rs
+++ crates/nexus/src/query/test/mod.rs
@@ -1,4 +1,6 @@
-use super::model::ReadModel;
+use super::{model::ReadModel, repository::ReadModelRepository};
+use std::pin::Pin;
+use thiserror::Error;
 
 // Read model
 pub struct User {
@@ -11,5 +13,34 @@ impl ReadModel for User {
 
     fn id(&self) -> &Self::Id {
         &self.id
+    }
+}
+
+#[derive(Debug, Error, Clone)]
+pub enum QueryError {
+    #[error("User not found")]
+    UserNotFound,
+}
+
+#[derive(Clone)]
+pub struct GetUserRepository;
+
+impl ReadModelRepository for GetUserRepository {
+    type Error = QueryError;
+    type Model = User;
+    fn get<'a>(
+        &'a self,
+        id: &'a <Self::Model as ReadModel>::Id,
+    ) -> Pin<Box<dyn Future<Output = Result<Self::Model, Self::Error>> + Send + 'a>> {
+        Box::pin(async move {
+            if id == "1" {
+                Ok(User {
+                    id: "1".to_string(),
+                    email: "joel@tixlys.com".to_string(),
+                })
+            } else {
+                Err(QueryError::UserNotFound)
+            }
+        })
     }
 }
