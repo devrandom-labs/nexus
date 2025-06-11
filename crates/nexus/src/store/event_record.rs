d486672eccd8426bb841c6f9605c4a5d04b49c47
Joel DSouza <joeldsouzax@gmail.com>
2025-06-11 23:44:15 +0200

    feat(nexus): even record with serialization

--- crates/nexus/src/store/event_record.rs
+++ crates/nexus/src/store/event_record.rs
@@ -2,6 +2,7 @@
 use crate::{DomainEvent, Id};
 use serde::{Deserialize, Serialize};
 use std::default::Default;
+use thiserror::Error;
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -25,6 +26,14 @@ impl<I> EventRecord<I>
 where
     I: Id,
 {
+    pub(crate) fn new(stream_id: I, version: u64, payload: Vec<u8>) -> Self {
+        EventRecord {
+            id: EventRecordId::default(),
+            stream_id,
+            version,
+            payload,
+        }
+    }
     pub fn builder<D>(domain_event: D) -> EventRecordBuilder<D, I>
     where
         D: DomainEvent<Id = I>,
@@ -57,10 +66,14 @@ where
     pub fn with_version(self, version: u64) -> EventRecordBuilderWithVersion<D, I> {
         EventRecordBuilderWithVersion {
             initial_event_record: self,
-            version: version,
+            version,
         }
     }
 }
+
+#[derive(Error, Debug)]
+#[error("PayloadSerializationError")]
+pub struct PayloadSerializationError;
 
 pub struct EventRecordBuilderWithVersion<D, I>
 where
@@ -69,4 +82,22 @@ where
 {
     initial_event_record: EventRecordBuilder<D, I>,
     version: u64,
+}
+
+impl<D, I> EventRecordBuilderWithVersion<D, I>
+where
+    I: Id,
+    D: DomainEvent<Id = I>,
+{
+    pub fn build<S>(self, serializer: S) -> Result<EventRecord<I>, PayloadSerializationError>
+    where
+        S: Fn(D) -> Result<Vec<u8>, PayloadSerializationError>,
+    {
+        let EventRecordBuilder {
+            stream_id,
+            domain_event,
+        } = self.initial_event_record;
+        let payload = serializer(domain_event)?;
+        Ok(EventRecord::new(stream_id, self.version, payload))
+    }
 }
