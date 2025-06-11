97b4477b96fc998b55acbc76caf7c25fe22a3790
Joel DSouza <joeldsouzax@gmail.com>
2025-06-12 00:30:43 +0200

    fix(nexus): concurrency check on append_events

--- crates/nexus/src/store/event_store.rs
+++ crates/nexus/src/store/event_store.rs
@@ -5,16 +5,19 @@ use tower::BoxError;
 
 pub trait EventStore {
     #[allow(clippy::type_complexity)]
-    fn append_events<E, I>(
-        event_records: E,
+    fn append_events<I>(
+        &self,
+        stream_id: &I,
+        expected_version: u64, // The crucial parameter for the concurrency check
+        event_records: Vec<EventRecord<I>>,
     ) -> Pin<Box<dyn Future<Output = Result<(), BoxError>> + Send + 'static>>
     where
-        E: IntoIterator<Item = EventRecord<I>>,
         I: Id;
 
     #[allow(clippy::type_complexity)]
     fn fetch_events<I>(
-        stream_id: I,
+        &self,
+        stream_id: &I,
     ) -> Pin<Box<dyn Future<Output = Result<Vec<EventRecord<I>>, BoxError>> + Send + Sync + 'static>>
     where
         I: Id;
