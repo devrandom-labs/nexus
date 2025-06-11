0953d58198e01fe7cc2bdb8c13fb3597038935ea
Joel DSouza <joeldsouzax@gmail.com>
2025-06-11 14:53:04 +0200

    feat(nexus): added lowlevel event record

--- crates/nexus/src/store/event_record.rs
+++ crates/nexus/src/store/event_record.rs
@@ -1,1 +1,36 @@
-pub struct EventRecord {}
+#![allow(dead_code)]
+use crate::Id;
+use serde::{Deserialize, Serialize};
+use std::default::Default;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EventRecordId(Uuid);
+
+impl Default for EventRecordId {
+    fn default() -> Self {
+        EventRecordId(Uuid::now_v7())
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EventRecord<I: Id> {
+    id: EventRecordId,
+    stream_id: I,
+    version: u64,
+    payload: Vec<u8>,
+}
+
+impl<I> EventRecord<I>
+where
+    I: Id,
+{
+    pub fn new(stream_id: I, payload: Vec<u8>) -> Self {
+        EventRecord {
+            id: EventRecordId::default(),
+            stream_id,
+            version: 0,
+            payload,
+        }
+    }
+}
