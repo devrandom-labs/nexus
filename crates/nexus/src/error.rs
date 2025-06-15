bacf1473d710ff4318e28039b21747011c657b28
Joel DSouza <joeldsouzax@gmail.com>
2025-06-15 16:34:46 +0200

    feat(nexus): global error with store error

--- crates/nexus/src/error.rs
+++ crates/nexus/src/error.rs
@@ -1,1 +1,7 @@
+use thiserror::Error as Err;
 
+#[derive(Debug, Err)]
+pub enum Error {
+    #[error("Store Error")]
+    Store,
+}
--- crates/nexus/src/store/event_store.rs
+++ crates/nexus/src/store/event_store.rs
@@ -1,20 +1,19 @@
 use super::event_record::EventRecord;
-use crate::Id;
+use crate::{Id, error::Error};
 use async_trait::async_trait;
-use tower::BoxError;
 
 #[async_trait]
 pub trait EventStore {
-    async fn append_events<I>(
+    async fn append_to_stream<I>(
         &self,
         stream_id: &I,
         expected_version: u64,
         event_records: Vec<EventRecord<I>>,
-    ) -> Result<(), BoxError>
+    ) -> Result<(), Error>
     where
         I: Id;
 
-    async fn read_stream_forward<I>(&self, stream_id: &I) -> Result<Vec<EventRecord<I>>, BoxError>
+    async fn read_stream<I>(&self, stream_id: &I) -> Result<Vec<EventRecord<I>>, Error>
     where
         I: Id;
 }
