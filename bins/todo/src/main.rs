10348d7e985513c7a722d4de132558afe55f73ef
Joel DSouza <joeldsouzax@gmail.com>
2025-06-15 22:00:30 +0200

    chore(todo): added event record schema

--- bins/todo/src/main.rs
+++ bins/todo/src/main.rs
@@ -2,57 +2,7 @@ use rusqlite::Result;
 
 mod store;
 
-// event record
-#[derive(Debug)]
-struct Person {
-    #[allow(dead_code)]
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
-}
-
 fn main() -> Result<()> {
-    // setup
-    let store = store::Store::new()?;
-
-    // one time bootstrap, since its in memory, we need this everytime.
-    store.connection.execute(
-        "CREATE TABLE person (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            data BLOB
-        )",
-        (),
-    )?;
-
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-
-    // part of EventStore append
-    store.connection.execute(
-        "INSERT INTO person(name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    )?;
-
-    // part of EventStore fetch
-    let mut stmt = store
-        .connection
-        .prepare("SELECT id, name, data FROM person")?;
-
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person?);
-    }
-
+    let _store = store::Store::new()?;
     Ok(())
 }
