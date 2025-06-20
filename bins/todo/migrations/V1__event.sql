7a7711b10a49b4403157952211de0e0260d31c16
Joel DSouza <joeldsouzax@gmail.com>
2025-06-20 23:15:13 +0200

    fix(todo): removed metdata blob

--- bins/todo/migrations/V1__event.sql
+++ bins/todo/migrations/V1__event.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS event (
        event_type TEXT NOT NULL,
        payload BLOB NOT NULL,
        persisted_at TEXT NOT NULL DEFAULT (datetime('now', 'utc')),
-       metadata BLOB,
        UNIQUE (stream_id, version)
 );
 
