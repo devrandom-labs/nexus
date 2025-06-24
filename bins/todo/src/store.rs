ce3d95fec04d5c3aadb0f2baaa2fad00ec00f479
Joel DSouza <joeldsouzax@gmail.com>
2025-06-24 22:09:06 +0200

    test(todo): added migration tests

--- bins/todo/src/store.rs
+++ bins/todo/src/store.rs
@@ -17,27 +17,11 @@ pub struct Store {
     pub connection: Arc<Mutex<Connection>>,
 }
 
-// TODO: evaluate refinery
-
 impl Store {
     #[instrument]
     pub fn new() -> Result<Self, SqlError> {
         debug!("initializing store connection...");
         let connection = Connection::open_in_memory()?;
-        let result = connection.execute(
-            "CREATE TABLE IF NOT EXISTS events (
-                 id TEXT PRIMARY KEY,
-                 stream_id TEXT NOT NULL,
-                 version INTEGER NOT NULL CHECK (version > 0),
-                 event_type TEXT NOT NULL,
-                 payload BLOB NOT NULL,
-                 persisted_at TEXT NOT NULL DEFAULT (datetime('now', 'utc')),
-                 metadata BLOB,
-                 UNIQUE (stream_id, version)
-            )",
-            (),
-        )?;
-        debug!("nexus_events table created: {}", result);
         Ok(Store {
             connection: Arc::new(Mutex::new(connection)),
         })
@@ -74,13 +58,54 @@ impl EventStore for Store {
 
 #[cfg(test)]
 mod tests {
+    use rusqlite::{Connection, Result};
+    use std::collections::HashSet;
 
-    // TODO: test schema is valid
-    // TODO: pragma foreign key.
+    mod embedded {
+        use refinery::embed_migrations;
+        embed_migrations!("migrations");
+    }
+
+    fn return_table_fields(table_name: &str, conn: &mut Connection) -> Result<HashSet<String>> {
+        let mut stm = conn
+            .prepare(&format!("PRAGMA table_info('{}')", table_name))
+            .unwrap();
+
+        stm.query_map([], |row| row.get(1))
+            .unwrap()
+            .collect::<Result<HashSet<String>>>()
+    }
 
     #[test]
-    fn should_have_correct_schema_and_is_valid() {}
+    fn should_have_events_table() {
+        let mut conn = Connection::open_in_memory().unwrap();
+        embedded::migrations::runner().run(&mut conn).unwrap();
+        let actual_fields = return_table_fields("event", &mut conn).unwrap();
+        assert!(!actual_fields.is_empty(), "migration has not been applied.");
+        for &expected_field in &[
+            "id",
+            "stream_id",
+            "version",
+            "event_type",
+            "payload",
+            "persisted_at",
+        ] {
+            assert!(actual_fields.contains(expected_field));
+        }
+    }
 
     #[test]
-    fn should_be_able_to_store_event_records() {}
+    fn should_have_event_metadata_table() {
+        let mut conn = Connection::open_in_memory().unwrap();
+        embedded::migrations::runner().run(&mut conn).unwrap();
+        let actual_fields = return_table_fields("event_metadata", &mut conn).unwrap();
+        assert!(!actual_fields.is_empty(), "migration has not been applied.");
+        for &expected_field in &["event_id", "correlation_id"] {
+            assert!(actual_fields.contains(expected_field));
+        }
+    }
+    #[test]
+    fn should_have_unique_contraint_on_stream_id_and_version() {}
+    #[test]
+    fn should_have_foreign_key_config_on() {}
 }
