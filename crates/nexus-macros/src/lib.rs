2aed8c256a186bbe1c6842a10fe4d31d633e1bda
Joel DSouza <joeldsouzax@gmail.com>
2025-06-30 22:33:47 +0200

    chore(macros): added skeleton for other core types

--- crates/nexus-macros/src/lib.rs
+++ crates/nexus-macros/src/lib.rs
@@ -1,18 +1,11 @@
 // TODO: create attribute macro for DomainEvent trait called nexus::
-use proc_macro::TokenStream;
+use proc_macro::{Ident, TokenStream};
 use quote::quote;
-use syn::{DeriveInput, parse_macro_input};
+use syn::{DeriveInput, Type, parse_macro_input};
 
 #[proc_macro_derive(Message)]
 pub fn message(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    let ident = ast.ident;
-    quote! {
-        impl ::nexus::core::Message for #ident {
-
-        }
-    }
-    .into()
 }
 
 #[proc_macro_derive(Command, attributes(command))]
@@ -31,4 +24,35 @@ pub fn query(input: TokenStream) -> TokenStream {
     let _struct_name = &ast.ident;
     eprintln!("{:#?}", ast);
     TokenStream::new()
+}
+
+#[proc_macro_derive(DomainEvent, attributes(event))]
+pub fn event(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    let _struct_name = &ast.ident;
+    eprintln!("{:#?}", ast);
+    TokenStream::new()
+}
+
+fn impl_message(ident: &Ident) -> TokenStream {
+    quote! {
+        impl ::nexus::core::Message for #ident {
+
+        }
+    }
+    .into()
+}
+
+struct CommandAttribute {
+    result: Type,
+    error: Type,
+}
+
+struct QueryAttribute {
+    result: Type,
+    error: Type,
+}
+
+struct DomainEventAttribute {
+    id: Type,
 }
