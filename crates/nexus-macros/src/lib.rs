171ffdfadc914790effa02c4b7273fd3405fdb7d
Joel DSouza <joeldsouzax@gmail.com>
2025-07-01 19:32:58 +0200

    feat(macros): added query macro

--- crates/nexus-macros/src/lib.rs
+++ crates/nexus-macros/src/lib.rs
@@ -15,6 +15,22 @@ pub fn command(input: TokenStream) -> TokenStream {
     .into()
 }
 
+#[proc_macro_derive(Query, attributes(query))]
+pub fn query(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    match parse_query(&ast) {
+        Ok(code) => code,
+        Err(e) => e.to_compile_error(),
+    }
+    .into()
+}
+
+#[proc_macro_derive(DomainEvent, attributes(domain_event))]
+pub fn domain_event(input: TokenStream) -> TokenStream {
+    let _ast = parse_macro_input!(input as DeriveInput);
+    unimplemented!()
+}
+
 fn parse_command(ast: &DeriveInput) -> Result<proc_macro2::TokenStream> {
     let name = &ast.ident;
     let attribute = utils::get_attribute(&ast.attrs, "command", name.span())?;
@@ -41,6 +57,45 @@ fn parse_command(ast: &DeriveInput) -> Result<proc_macro2::TokenStream> {
 
     let expanded = quote! {
         impl ::nexus::core::Command for #name {
+            type Result = #result;
+            type Error = #error_type;
+
+        }
+
+        impl ::nexus::core::Message for #name {
+
+        }
+    }
+    .into();
+
+    Ok(expanded)
+}
+
+fn parse_query(ast: &DeriveInput) -> Result<proc_macro2::TokenStream> {
+    let name = &ast.ident;
+    let attribute = utils::get_attribute(&ast.attrs, "query", name.span())?;
+    let mut result: Option<Type> = None;
+    let mut error_type: Option<Type> = None;
+
+    attribute.parse_nested_meta(|meta| {
+        if meta.path.is_ident("result") {
+            result = Some(meta.value()?.parse()?);
+        } else if meta.path.is_ident("error") {
+            error_type = Some(meta.value()?.parse()?);
+        } else {
+            return Err(meta.error("unrecognized key for `#[query]` attribute"));
+        }
+        Ok(())
+    })?;
+
+    let result =
+        result.ok_or_else(|| Error::new(attribute.path().span(), "`result` key is required"))?;
+
+    let error_type =
+        error_type.ok_or_else(|| Error::new(attribute.path().span(), "`error` key is required"))?;
+
+    let expanded = quote! {
+        impl ::nexus::core::Query for #name {
             type Result = #result;
             type Error = #error_type;
 
