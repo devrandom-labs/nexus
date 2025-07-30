0f1650c73a3a45058e50f62fc1de3c139786b6fb
Joel DSouza <joeldsouzax@gmail.com>
2025-07-30 14:47:01 +0200

    feat(helpers): added arbitrary sequence of events

--- crates/nexus-rusqlite/tests/store_properties.rs
+++ crates/nexus-rusqlite/tests/store_properties.rs
@@ -1,1 +1,11 @@
+use nexus_test_helpers::pending_event_strategies::arbitrary_valid_sequence;
+use proptest::prelude::*;
 
+proptest! {
+    #[test]
+    fn prop_can_append_a_valid_sequence( _events in arbitrary_valid_sequence()) {
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            unimplemented!()
+        })
+    }
+}
--- crates/nexus-test-helpers/src/pending_event_strategies.rs
+++ crates/nexus-test-helpers/src/pending_event_strategies.rs
@@ -1,32 +1,61 @@
-use nexus::{event::PendingEvent, infra::NexusId};
+use nexus::{
+    event::{EventMetadata, PendingEvent},
+    infra::{CorrelationId, NexusId},
+};
 use proptest::prelude::*;
 
 pub type TestPendingEvent = PendingEvent<NexusId>;
 
-// TODO: valid_sequence
 // TODO: invalid_sequence
 // TODO: multiple_stream_valid_sequence
 // TODO: multiple_stream_invalid_sequence
 
-fn valid_sequence() -> impl Strategy<Value = Vec<TestPendingEvent>> {
-    // stream_id strategy
-    let stream_id_strategy = any::<[u8; 16]>().prop_map(|bytes| {
+pub fn arbitrary_correlation_id() -> impl Strategy<Value = CorrelationId> {
+    any::<[u8; 16]>().prop_map(|bytes| {
+        let uuid = uuid::Builder::from_random_bytes(bytes)
+            .into_uuid()
+            .to_string();
+        Into::<CorrelationId>::into(uuid)
+    })
+}
+
+pub fn arbitrary_event_metadata() -> impl Strategy<Value = EventMetadata> {
+    arbitrary_correlation_id().prop_map(|correlation_id| EventMetadata::new(correlation_id))
+}
+
+pub fn arbitrary_stream_id() -> impl Strategy<Value = NexusId> {
+    any::<[u8; 16]>().prop_map(|bytes| {
         let uuid = uuid::Builder::from_random_bytes(bytes).into_uuid();
         Into::<NexusId>::into(uuid)
-    });
+    })
+}
 
-    (stream_id_strategy, 0..10_usize)
+pub fn arbitrary_valid_sequence() -> impl Strategy<Value = Vec<TestPendingEvent>> {
+    // stream_id strategy
+    (arbitrary_stream_id(), 0..10_usize)
         .prop_flat_map(|(stream_id, num_events)| {
             (
                 Just(stream_id),
-                prop::collection::vec((any::<String>(), any::<Vec<u8>>()), num_events),
+                prop::collection::vec(
+                    (
+                        arbitrary_event_metadata(),
+                        any::<String>(),
+                        any::<Vec<u8>>(),
+                    ),
+                    num_events,
+                ),
             )
         })
         .prop_map(|(stream_id, other_data)| {
             other_data
                 .into_iter()
                 .enumerate()
-                .map(|(index, (event_type, payload))| todo!())
+                .map(|(index, (metadata, event_type, payload))| {
+                    PendingEvent::builder(stream_id)
+                        .with_version((index + 1) as u64)
+                        .with_metadata(metadata)
+                        .build_with_payload(payload, event_type)
+                })
                 .collect::<Vec<_>>()
         })
 }
