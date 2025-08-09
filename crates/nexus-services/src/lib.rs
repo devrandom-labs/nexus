a5c29af34a96b55540334bbb91ccbdac5d25b701
Joel DSouza <joeldsouzax@gmail.com>
2025-08-09 18:54:04 +0200

    chore(nexus): added terrors

--- crates/nexus-services/src/lib.rs
+++ crates/nexus-services/src/lib.rs
@@ -4,17 +4,75 @@ use nexus::{
     domain::{Aggregate, AggregateState, Command},
 };
 
+pub enum DispatchOutcome<A, C>
+where
+    A: Aggregate,
+    C: Command,
+{
+    Handled(
+        Result<CommandHandlerResponse<<A::State as AggregateState>::Domain, C::Error>, C::Error>,
+    ),
+    Unhandled(C),
+}
+
 #[async_trait]
-pub trait CommandHandlerSet<A, C, S>
+pub trait Dispatch<A, C, S>
 where
     A: Aggregate,
     C: Command,
     S: Send + Sync + ?Sized,
 {
-    async fn handle_command(
-        &self,
-        state: &A::State,
-        command: C,
-        services: &S,
-    ) -> Result<CommandHandlerResponse<<A::State as AggregateState>::Domain, C::Result>, C::Error>;
+    async fn dispatch(&self, state: &A::State, command: C, services: &S) -> DispatchOutcome<A, C>;
 }
+
+#[async_trait]
+impl<A, C, S> Dispatch<A, C, S> for ()
+where
+    A: Aggregate,
+    C: Command,
+    S: Send + Sync + ?Sized,
+{
+    async fn dispatch(&self, state: &A::State, command: C, services: &S) -> DispatchOutcome<A, C> {
+        Err(command)
+    }
+}
+
+// #[async_trait]
+// impl<A, C, S, H, T> Dispatch<A, C, S> for (H, T)
+// where
+//     A: Aggregate,
+//     C: Command,
+//     S: Send + Sync + ?Sized,
+//     H: AggregateCommandHandler<C, S, State = A::State>,
+//     T: Send + Sync,
+// {
+//     async fn dispatch(
+//         &self,
+//         state: &A::State,
+//         command: C,
+//         services: &S,
+//     ) -> Result<CommandHandlerResponse<<A::State as AggregateState>::Domain, C::Result>, C::Error>
+//     {
+//         self.0.handle(state, command, services).await
+//     }
+// }
+
+// #[async_trait]
+// impl<A, C, S, H, T> CommandHandlerSet<A, C, S> for (H, T)
+// where
+//     A: Aggregate,
+//     C: Command,
+//     S: Send + Sync + ?Sized,
+//     T: CommandHandlerSet<A, C, S>,
+//     H: Send + Sync,
+// {
+//     async fn handle_command(
+//         &self,
+//         state: &A::State,
+//         command: C,
+//         services: &S,
+//     ) -> Result<CommandHandlerResponse<<A::State as AggregateState>::Domain, C::Result>, C::Error>
+//     {
+//         self.1.handle_command(state, command, services).await
+//     }
+// }
