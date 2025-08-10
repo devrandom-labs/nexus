157df2289af3b4b13a1e3170c5e8c896c9be48c1
Joel DSouza <joeldsouzax@gmail.com>
2025-08-10 15:50:31 +0200

    feat(service): added aggregate service

--- crates/nexus-services/src/lib.rs
+++ crates/nexus-services/src/lib.rs
@@ -1,71 +1,32 @@
-use async_trait::async_trait;
-use nexus::{
-    command::{AggregateCommandHandler, CommandHandlerResponse},
-    domain::{Aggregate, AggregateState, Command},
-};
-use terrors::OneOf;
+use nexus::{command::EventSourceRepository, domain::Aggregate};
+use std::{marker::PhantomData, sync::Arc};
 
-pub struct Handler<T>(pub T);
-
-#[async_trait]
-pub trait Dispatch<A, C, S>
+pub struct AggregateService<A, R, S>
 where
     A: Aggregate,
-    C: Command,
-    S: Send + Sync + ?Sized,
+    R: EventSourceRepository<Aggregate = A>,
+    S: Send + Sync,
 {
-    async fn dispatch(
-        &self,
-        state: &A::State,
-        command: C,
-        services: &S,
-    ) -> Result<
-        CommandHandlerResponse<<A::State as AggregateState>::Domain, C::Result>,
-        OneOf<(C::Error, C)>,
-    >;
+    repository: Arc<R>,
+    service: Option<Arc<S>>,
+    _marker: PhantomData<A>,
 }
 
-#[async_trait]
-impl<A, C, S, H> Dispatch<A, C, S> for Handler<H>
+impl<A, R, S> AggregateService<A, R, S>
 where
     A: Aggregate,
-    C: Command,
-    S: Send + Sync + ?Sized,
-    H: AggregateCommandHandler<C, S, State = A::State>,
+    R: EventSourceRepository<Aggregate = A>,
+    S: Send + Sync,
 {
-    async fn dispatch(
-        &self,
-        state: &A::State,
-        command: C,
-        services: &S,
-    ) -> Result<
-        CommandHandlerResponse<<A::State as AggregateState>::Domain, C::Result>,
-        OneOf<(C::Error, C)>,
-    > {
-        Ok(self
-            .0
-            .handle(state, command, services)
-            .await
-            .map_err(OneOf::new)?)
+    pub fn new(repo: R) -> Self {
+        AggregateService {
+            repository: Arc::new(repo),
+            service: None,
+            _marker: PhantomData,
+        }
     }
-}
 
-#[async_trait]
-impl<A, C, S> Dispatch<A, C, S> for ()
-where
-    A: Aggregate,
-    C: Command,
-    S: Send + Sync + ?Sized,
-{
-    async fn dispatch(
-        &self,
-        _state: &A::State,
-        command: C,
-        _services: &S,
-    ) -> Result<
-        CommandHandlerResponse<<A::State as AggregateState>::Domain, C::Result>,
-        OneOf<(C::Error, C)>,
-    > {
-        Err(OneOf::new(command))
+    pub fn with_service(&mut self, service: S) {
+        self.service.replace(Arc::new(service));
     }
 }
